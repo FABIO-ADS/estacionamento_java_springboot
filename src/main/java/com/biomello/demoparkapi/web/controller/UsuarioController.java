3512d028755c370522588028d918d31f534f6f83
unknown <you@example.com>
2023-10-25 21:07:33 -0300

    Recurso para localizar um usuário pelo id

--- src/main/java/com/biomello/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/biomello/demoparkapi/web/controller/UsuarioController.java
@@ -5,10 +5,7 @@ import com.biomello.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +19,13 @@ public class UsuarioController {
         Usuario user = usuarioService.salvar(usuario);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+
+        Usuario user = usuarioService.buscarPorId(id);
+
+        return ResponseEntity.ok(user);
     }
 }
